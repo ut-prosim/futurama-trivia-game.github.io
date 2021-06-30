245cb6c33ac0d06e9e2ffc2b64e9d8f117bf1c4b
brian kasten <panphobicdream@gmail.com>
2021-06-30 09:34:05 -0400

    start button hides after pressing it

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+$("#begin").click(function () {
+    this.style.visibility = "hidden";
+})
+
 async function getFuturamaQs(e) {
     e.preventDefault();
 
@@ -12,6 +16,10 @@ async function getFuturamaQs(e) {
         // let question = data[i].question;
         // let possibleAnswers = data[i].possibleAnswers;
         // let correctAnswer = data[i].correctAnswer;
+        // const random = Math.floor(Math.random() * 28);
+
+        // console.log(random);
+        // i = random
         $('#question').html(data[i].question);
         $('#possibleAnswers').html(data[i].possibleAnswers);
     }
