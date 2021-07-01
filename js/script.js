29fb53ef2ddcbfd02ca70807f50005e7ad25ae1b
brian kasten <panphobicdream@gmail.com>
2021-06-30 20:10:07 -0400

    last chance

--- js/script.js
+++ js/script.js
@@ -1,21 +1,9 @@
 $("#begin").click(function () {
     this.style.visibility = "hidden";
-})
-let correct = 0;
-let incorrect = 0;
-let question = null;
-let guess = null;
-let correctAnswer = null;
+});
 
-let score = function() {
-    if (guess == correctAnswer) {
-        correct += 1;
-        alert('correct');
-    }  else {
-        incorrect += 1;
-        alert('incorrect');
-    }
-};
+alert('welcome to the world of tomorrow! ...ahem, also known as the 31st century!');
+
 
 async function getFuturamaQs(e) {
     e.preventDefault();
@@ -26,17 +14,48 @@ async function getFuturamaQs(e) {
     const data = await response.json();
    
     console.log(data);
-    
-    for (let i=0; i < data.length; i++) {
-        // let question = data[i].question;
-        // let possibleAnswers = data[i].possibleAnswers;
-        // let correctAnswer = data[i].correctAnswer;
-        // const random = Math.floor(Math.random() * 28);
 
+    let correct = 0;
+    let incorrect = 0;
+    let question = null;
+    let input = null;
+    let correctAnswer = null;
+    
+    
+    
+    for (let i=0; i < data.length-1; i++) {
+        let question = data[i].question;
+        console.log(question);
+        let possibleAnswers = data[i].possibleAnswers;
+        let correctAnswer = data[i].correctAnswer;
+        // const random = Math.floor(Math.random() * 28);
         // console.log(random);
-        // i = random
+
+
         $('#question').html(data[i].question);
         $('#possibleAnswers').html(data[i].possibleAnswers);
+
+
+        let ask = function() { 
+            input = prompt('#question');
+        };
+        
+        let score = function() {
+            if (input == correctAnswer) {
+                correct += 1;
+                alert('correct');
+            }  else {
+                incorrect += 1;
+                alert('incorrect');
+            }
+        };
+    
+        const continueGame = function() {
+            ask();
+            score();
+        };
+        
+    
     }
 
 }
