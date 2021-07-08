3357d9a837675e21d16bfc0278acdf2c700d9a05
brian kasten <panphobicdream@gmail.com>
2021-07-08 00:31:26 -0400

    added array of previos questions

--- js/script.js
+++ js/script.js
@@ -1,48 +1,46 @@
 (async () => {
-$('#begin').click(function () {
-    this.style.visibility = 'hidden';
-    displayNewQuestion();
-});
-$('#answer').click(function () {
+    $('#begin').click(function () {
+        this.style.visibility = 'hidden';
+        displayNewQuestion();
+    });
+    $('#answer').click(function () {
 
-});
+    });
 
-alert('welcome to the world of tomorrow! ...ahem, also known as the 31st century!');
+    alert('welcome to the world of tomorrow! ...ahem, also known as the 31st century!');
 
-const url = `https://api.sampleapis.com/futurama/questions`;
-const response = await fetch(url);
-const data = await response.json();
+    const url = `https://api.sampleapis.com/futurama/questions`;
+    const response = await fetch(url);
+    const data = await response.json();
    
     console.log(data);
 
-let correct = 0;
-let incorrect = 0;
-let questionCount = 0;
+    let correct = 0;
+    let incorrect = 0;
+    let questionCount = 0;
     
-function displayNewQuestion() {
-    let randomIndex = randNum();
-    let question = data[randomIndex];
+    function displayNewQuestion() {
+        let randomIndex = randNum();
+        let question = data[randomIndex];
+        let previousQuestions = [];
 
-    $('#question').html(data[randomIndex].question);
-    
-    question.possibleAnswers.forEach(answer => {
-        $('#possibleAnswers').append(`<button id='answer'>${answer}</button>`);
-    });
-}
-
-    // let question = data[i].question;
-    // console.log(question);
-    // let possibleAnswers = data[i].possibleAnswers;
-    // let correctAnswer = data[i].correctAnswer;
-    // const random = 
-    // console.log(random);
-
-function randNum() {
-    return Math.floor(Math.random() * 28);
-}
+        $('#question').html(data[randomIndex].question);
         
+        question.possibleAnswers.forEach(answer => {
+            $('#possibleAnswers').append(`<button id='answer'>${answer}</button>`);
+        })
     
+        $('#possibleAnswers').on('click', function (e) {
+            $('#question').push(previousQuestions);
+            console.log(question);
+        })    
+    }
 
+
+
+    function randNum() {
+        return Math.floor(Math.random() * 28);
+    }
 
 
 
